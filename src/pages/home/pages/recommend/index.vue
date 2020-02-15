b4a8ea3be277abfe553b6bf529c33e67a1e8870d
王茂文 <3277954078@qq.com>
2020-02-15 19:45:59 +0800

    nav组件完成

--- src/pages/home/pages/recommend/index.vue
+++ src/pages/home/pages/recommend/index.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="recommend">
-
-      <!-- 推荐页轮播图组件 -->
-      <recommend-slider :swiperList="swiperList"></recommend-slider>
-
+    <!-- 推荐页轮播图组件 -->
+    <recommend-slider :swiperList="swiperList"></recommend-slider>
+    <!-- 推荐页nav组件 -->
+    <recommend-nav :iconList="iconList"></recommend-nav>
+    <!-- 广告图片和商品展示部分 -->
+    <!-- bannerList部分 -->
+    <div class="bannerList" v-if="bannerList.length">
+      <div class="left-items">
+        <a href="#">
+          <img :src="bannerList[0].imgUrl" />
+        </a>
+      </div>
+      <div class="right-items">
+        <a href="#" class="right-item">
+          <img :src="bannerList[1].imgUrl" />
+        </a>
+        <a href="#">
+          <img :src="bannerList[2].imgUrl" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import RecommendSlider from './slider';
+  import RecommendNav from './nav';
 
   import { SUCC_CODE } from 'api/config.js';
 
   export default {
     name: 'Recommend',
     components: {
-      RecommendSlider
-
+      RecommendSlider,
+      RecommendNav
     },
     data() {
       return {
@@ -79,6 +97,7 @@
 .recommend {
   position: relative;
   height: 100%;
+  background-color: $bgc-recom;
 }
 .wrapper {
   width: 7.2rem;
@@ -95,6 +114,7 @@
   display: flex;
   width: 7.2rem;
   height: 5.08rem;
+  margin-top: 0.16rem;
   div {
     flex: 1;
     a {
@@ -110,290 +130,5 @@
   .right-item {
     margin-bottom: 0.04rem;
   }
-}
-// bannerList01部分
-.bannerList01 {
-  a {
-    display: block;
-    width: 7.2rem;
-  }
-  .bannerList01-top {
-    display: block;
-    width: 7.2rem;
-    height: 2.8rem;
-  }
-  .bannerList01-buttom {
-    display: block;
-    width: 7.2rem;
-    height: 4.4rem;
-  }
-}
-// shoplist01部分
-.shoplist01 {
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  padding: 0 0.12rem;
-  border-bottom: 0.02rem solid rgb(206, 206, 206);
-  .shop01-item {
-    width: 3.4rem;
-    &:nth-child(2n) {
-      margin-left: 0.12rem;
-      margin-bottom: 0.0666667rem;
-    }
-    &:last-child {
-      margin-bottom: 0;
-    }
-    a {
-      display: block;
-      .shop01-top {
-        width: 3.42rem;
-        overflow: hidden;
-        .top-img {
-          width: 3.44rem;
-          height: 2.8rem;
-        }
-      }
-      .shop01-buttom {
-        width: 3.42rem;
-        padding: 0.2rem 0.27rem;
-        text-align: center;
-        .name {
-          font-size: 0.28rem;
-          color: rgba(0, 0, 0, 0.87);
-          @include ellipsis();
-        }
-        .brief {
-          margin-top: 0.06rem;
-          font-size: 0.22rem;
-          line-height: 0.3rem;
-          color: rgba(0, 0, 0, 0.54);
-          @include ellipsis();
-        }
-        .price {
-          font-size: 0.28rem;
-          color: #ea625b;
-          height: 1.5em;
-          line-height: 1.5em;
-          position: relative;
-          display: inline-block;
-          padding-left: 0.5em;
-          &::before {
-            content: "\A5";
-            position: absolute;
-            left: 0;
-            top: 0;
-            font-size: 0.76em;
-          }
-          span:nth-child(2) {
-            display: inline-block;
-            margin-left: 0.04rem;
-            font-size: 0.2rem;
-          }
-          .org_price {
-            position: relative;
-            display: inline-block;
-            margin: 0 0.1rem;
-            padding-left: 0.5em;
-            font-size: 0.22rem !important;
-            color: rgba(0, 0, 0, 0.54);
-            &::before {
-              content: "\A5";
-              position: absolute;
-              left: 0;
-              top: 0;
-              font-size: 0.76em;
-            }
-          }
-        }
-        .btn {
-          width: 2rem;
-          background: #ea625b;
-          border-radius: 0.05rem;
-          text-align: center;
-          color: #fff;
-          font-size: 0.24rem;
-          padding: 0.16rem 0;
-          margin: 0 auto;
-          font-weight: 700;
-        }
-      }
-    }
-  }
-}
-// 更多米家产品部分
-.more-mi {
-  background: #fff;
-  height: 1rem;
-  line-height: 1rem;
-  font-size: 0.28rem;
-  text-align: center;
-  a {
-    display: block;
-  }
-}
-// shoplist04部分
-.shoplist04 {
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  border-bottom: 0.02rem solid rgb(206, 206, 206);
-  .shop04-item {
-    width: 3.6rem;
-    &:nth-child(2n) {
-      width: 3.5rem;
-      margin-left: 0.04rem;
-    }
-    a {
-      display: block;
-      .shop04-top {
-        width: 3.6rem;
-        height: 3.6rem;
-        .top-img {
-          display: block;
-          width: 3.6rem;
-          height: 3.6rem;
-        }
-      }
-      .shop01-buttom {
-        padding: 0.2rem 0.27rem;
-        text-align: left;
-        .name {
-          font-size: 0.28rem;
-          color: rgba(0, 0, 0, 0.87);
-          @include ellipsis();
-        }
-        .brief {
-          margin-top: 0.06rem;
-          font-size: 0.22rem;
-          line-height: 0.3rem;
-          color: rgba(0, 0, 0, 0.54);
-          @include ellipsis();
-        }
-        .price {
-          font-size: 0.28rem;
-          color: #ea625b;
-          height: 1.5em;
-          line-height: 1.5em;
-          position: relative;
-          display: inline-block;
-          padding-left: 0.5em;
-          &::before {
-            content: "\A5";
-            position: absolute;
-            left: 0;
-            top: 0;
-            font-size: 0.76em;
-          }
-          .org_price {
-            position: relative;
-            display: inline-block;
-            margin: 0 0.1rem;
-            padding-left: 0.5em;
-            font-size: 0.22rem;
-            color: rgba(0, 0, 0, 0.54);
-            &::before {
-              content: "\A5";
-              position: absolute;
-              left: 0;
-              top: 0;
-              font-size: 0.76em;
-            }
-          }
-        }
-      }
-    }
-  }
-}
-// shoplist05部分
-.shoplist05 {
-  border-bottom: 0.02rem solid rgb(206, 206, 206);
-  .shop05 {
-    width: 100%;
-    display: flex;
-    .left {
-      width: 3.6rem;
-      height: 3.6rem;
-      img {
-        display: block;
-        width: 3.6rem;
-        height: 3.6rem;
-      }
-    }
-    .right {
-      width: 3.6rem;
-      height: 3.6rem;
-      padding: 0.64rem 0.12rem 0 0.32rem;
-      text-align: left;
-      .name {
-        font-size: 0.28rem;
-        color: rgba(0, 0, 0, 0.87);
-        @include ellipsis();
-      }
-      .brief {
-        margin-top: 0.06rem;
-        font-size: 0.22rem;
-        line-height: 0.35rem;
-        color: rgba(0, 0, 0, 0.54);
-      }
-      .price {
-        font-size: 0.28rem;
-        color: #ea625b;
-        height: 1.5em;
-        line-height: 1.5em;
-        position: relative;
-        display: inline-block;
-        padding-left: 0.5em;
-        margin-top: 0.2rem;
-        &::before {
-          content: "\A5";
-          position: absolute;
-          left: 0;
-          top: 0;
-          font-size: 0.76em;
-        }
-        .org_price {
-          position: relative;
-          display: inline-block;
-          margin: 0 0.1rem;
-          padding-left: 0.5em;
-          font-size: 0.22rem;
-          color: rgba(0, 0, 0, 0.54);
-          &::before {
-            content: "\A5";
-            position: absolute;
-            left: 0;
-            top: 0;
-            font-size: 0.76em;
-          }
-        }
-      }
-    }
-  }
-}
-// shoplist06部分
-.shoplist06 {
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  a {
-    display: block;
-    img {
-      width: 3.58rem;
-      height: 4.8rem;
-    }
-    &:nth-child(2n) {
-      margin-left: 0.04rem;
-      margin-bottom: 0.04rem;
-    }
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-}
-// bannerList06部分
-.bannerList06-buttom {
-  height: 1.06rem;
-  width: 7.2rem;
 }
 </style>
---
+++ src/pages/home/pages/recommend/nav.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="nav">
+    <ul class="nav-list">
+      <li class="nav-item" v-for="(item, index) in iconList" :key="index">
+        <a :href="item.path" class="nav-link">
+          <img :src="item.imgUrl" class="nav-pic" />
+          <!-- <span>{{}}</span> -->
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'RecommendNav',
+    props: {
+      iconList: Array
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  width: 100%;
+  height: 3.04rem;
+  &-list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  &-item {
+    width: 20%;
+  }
+
+  &-link {
+    display: block;
+  }
+
+  &-pic {
+    width: 100%;
+  }
+}
+</style>
